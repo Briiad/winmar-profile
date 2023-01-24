c2cead5ff7a3e16804afbaab14ab5e0ca5a788b5
Brian Aditya <brianadit33@gmail.com>
2023-01-24 18:41:46 +0700

    Initiallize

---
+++ components/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <section className='w-full h-screen flex items-center justify-center font-bold'>
+      <h1>This is the company profile of PT. Widiji Nusantara Makmur</h1>
+    </section>
+  )
+}
+
+export default LandingPage
