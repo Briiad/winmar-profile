139a40139ffc7c6af302c498343a32486c8efb99
Brian Aditya <brianadit33@gmail.com>
2023-02-07 13:22:38 +0700

    v1.1.2 - Hotfix

--- components/Documentation.js
+++ components/Documentation.js
@@ -8,21 +8,21 @@ const Documentation = () => {
         <h1 className='font-playfair text-6xl md:text-7xl text-primary-400 flex items-center text-center px-12'>"Our Hardwork, Made From Love."</h1>
       </div>
       <div className='h-full w-full max-w-7xl mx-auto gap-5 columns-2 md:columns-3 space-y-3 p-4 md:p-12'>
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc1.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc2.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc3.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc4.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc5.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc6.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc7.jpeg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc8.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc9.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc10.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc11.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc12.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc13.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc14.jpg'} alt="" className='shadow-lg' />
-        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc15.jpg'} alt="" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc1.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc2.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc3.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc4.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc5.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc6.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc7.jpeg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc8.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc9.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc10.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc11.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc12.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc13.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc14.jpg'} alt="doc image" className='shadow-lg' />
+        <motion.img whileHover={{scale: 1.05}} src={'/Assets/Images/Documentations/doc15.jpg'} alt="doc image" className='shadow-lg' />
       </div>
       
     </section>
